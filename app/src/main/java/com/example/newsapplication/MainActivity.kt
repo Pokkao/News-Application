200d78a7752a8e8984ff64a9a03a73bbf7e2de50
Pokkao <sooksantibanyon@hotmail.com>
2019-02-10 03:54:19 +0700

    RecycleView

--- app/src/main/java/com/example/newsapplication/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/MainActivity.kt
@@ -2,17 +2,42 @@ package com.example.newsapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    val news: ArrayList<String> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        a()
+        addNews()
+
+        recycler_view.setHasFixedSize(true)
+        recycler_view.layoutManager = LinearLayoutManager(this)
+
+
+        recycler_view.layoutManager = GridLayoutManager(this, 1)
+        recycler_view.adapter = AdapterRecycle(news,this)
     }
 
-    private fun a() {
-
+    fun addNews() {
+        news.add("dog")
+        news.add("cat")
+        news.add("owl")
+        news.add("cheetah")
+        news.add("raccoon")
+        news.add("bird")
+        news.add("snake")
+        news.add("ant")
+        news.add("bee")
+        news.add("tiger")
     }
+
+
 }
