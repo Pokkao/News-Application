d6050432f30fa278eb97e60ca36ad9b31103354c
Pokkao <sooksantibanyon@hotmail.com>
2019-03-28 22:01:38 +0700

    increate NavigationView(menu,main_activity) on MainActivity

--- app/src/main/java/com/example/newsapplication/module/activity/MainActivity.kt
+++ app/src/main/java/com/example/newsapplication/module/activity/MainActivity.kt
@@ -5,6 +5,10 @@ import android.app.PendingIntent.getActivity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.support.annotation.NonNull
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.Menu
@@ -14,25 +18,39 @@ import com.example.newsapplication.module.fragment.PageNewsFragment
 import com.example.newsapplication.R
 import com.example.newsapplication.module.fragment.PageNews
 import kotlinx.android.synthetic.main.fragment_page_news.*
-import kotlinx.android.synthetic.main.fragment_page_news2.*
-import kotlinx.android.synthetic.main.fragment_page_news2.view.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
 
     var recyclerViewState: Parcelable? = null
+
+
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         settitlebar()
+        setNavigation()
         ProfileNews()
+        clickoptionBar()
 
+    }
+
+    private fun setNavigation() {
+        nav_view!!.setNavigationItemSelectedListener(this@MainActivity)
+    }
+
+    private fun clickoptionBar() {
+        bt_option_menu.setOnClickListener {
+            drawer_layout.openDrawer(GravityCompat.START)
+        }
     }
 
 
     fun settitlebar() {
         setSupportActionBar(my_toolbar)
+//        supportActionBar!!.setDisplayHomeAsUpEnabled(true)  //arrow
         supportActionBar!!.setDisplayShowTitleEnabled(false)
     }
 
@@ -48,18 +66,33 @@ class MainActivity : AppCompatActivity() {
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
+        nav_view.setBackgroundResource(R.color.line_color_university)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         val id = item.itemId
-
         if (id == R.id.action_favorite) {
-            Toast.makeText(this@MainActivity, "Thank you for Click! :)", Toast.LENGTH_LONG).show()
+
+//            **drawer_layout.openDrawer(GravityCompat.START)
+//            Toast.makeText(this@MainActivity, "Thank you for Click! :)", Toast.LENGTH_LONG).show()
             return true
         }
 
         return super.onOptionsItemSelected(item)
+    }
+
+    override fun onNavigationItemSelected(@NonNull item: MenuItem): Boolean {
+        item.isChecked = true
+
+        when (item.itemId) {
+            R.id.menu_my_favorite -> Log.d(TAG, "onNavigationItemSelected: " + item.title)
+            R.id.menu_my_other -> Log.d(TAG, "onNavigationItemSelected: " + item.title)
+            R.id.menu_my_setting -> Log.d(TAG, "onNavigationItemSelected: " + item.title)
+
+        }
+        drawer_layout.closeDrawers()
+        return true
     }
 
 
